042cd116b547d141b525cad021d3d1b7b71ed3a0
paweld139 <pawell139139@gmail.com>
2022-10-09 00:42:19 +0200

    Fix

--- ReactTest/ClientApp/src/App.js
+++ ReactTest/ClientApp/src/App.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import Layout from './components/Layout';
 import './custom.css';
 
-export default class App extends Component {
-    static displayName = App.name;
+const App = () => (
+    <Layout module='Module1' children={
+        <Routes>
+            {AppRoutes.map((route, index) => {
+                const { element, requireAuth, ...rest } = route;
+                return <Route key={index} {...rest} element={requireAuth ? <AuthorizeRoute {...rest} element={element} /> : element} />;
+            })}
+        </Routes>}>
+    </Layout>
+);
 
-    render() {
-        return (
-            <Layout module='Module1' children={
-                <Routes>
-                    {AppRoutes.map((route, index) => {
-                        const { element, requireAuth, ...rest } = route;
-                        return <Route key={index} {...rest} element={requireAuth ? <AuthorizeRoute {...rest} element={element} /> : element} />;
-                    })}
-                </Routes>}>
-            </Layout>
-        );
-    }
-}
+export default App;
--- ReactTest/ClientApp/src/components/FetchData.js
+++ ReactTest/ClientApp/src/components/FetchData.js
@@ -1,37 +1,35 @@
 import React from 'react';
 import Table from './Table';
 
-const FetchData = () => {
-    return (
-        <Table
-            defaultColumn='date'
-            defaultOrder='desc'
-            defaultPage={1}
-            defaultPageSize={10}
-            action='weatherforecast'
-            label='Weather forecast'
-            description='This component demonstrates fetching data from the server.'
-            columns={[
-                {
-                    name: 'Date',
-                    key: 'date'
-                },
-                {
-                    name: 'Temp. (C)',
-                    key: 'temperatureC'
-                },
-                {
-                    name: 'Temp. (F)',
-                    key: 'temperatureF'
-                },
-                {
-                    name: 'Summary',
-                    key: 'summary'
-                }
-            ]}
-            rowKey='date'
-        />
-    );
-};
+const FetchData = () => (
+    <Table
+        defaultColumn='date'
+        defaultOrder='desc'
+        defaultPage={1}
+        defaultPageSize={10}
+        action='weatherforecast'
+        label='Weather forecast'
+        description='This component demonstrates fetching data from the server.'
+        columns={[
+            {
+                name: 'Date',
+                key: 'date'
+            },
+            {
+                name: 'Temp. (C)',
+                key: 'temperatureC'
+            },
+            {
+                name: 'Temp. (F)',
+                key: 'temperatureF'
+            },
+            {
+                name: 'Summary',
+                key: 'summary'
+            }
+        ]}
+        rowKey='date'
+    />
+);
 
 export default FetchData;
